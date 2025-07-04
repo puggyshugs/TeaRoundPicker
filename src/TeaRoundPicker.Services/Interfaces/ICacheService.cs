1c333fdf0be72ad43edaa81d48e36386bfe1c70b
George Collingridge <76482909+puggyshugs@users.noreply.github.com>
2025-07-04 14:19:39 +0100

    update icacheservice with new methods SetMultipleParticipants and GetAllParticipants

--- src/TeaRoundPicker.Services/Interfaces/ICacheService.cs
+++ src/TeaRoundPicker.Services/Interfaces/ICacheService.cs
@@ -4,6 +4,8 @@ namespace TeaRoundPicker.Services.Interfaces;
 
 public interface ICacheService
 {
+    List<Participant> GetAllParticipants();
     Participant GetParticipant(string key);
     string CreateParticipant(string name);
+    string CreateParticipants(List<string> names);
 }
